b602edc78064ead6ee080cb973892125e3b51e63
chilkaditya <apurkahini@gmail.com>
2024-04-10 21:21:05 +0530

    initialized project

---
+++ REST_API/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.get('/',(req,res)=>{
+    res.send("Hello world");
+})
+
+
+
+
+app.listen(8000,()=>{
+    console.log("Server is running on port:8000");
+})
